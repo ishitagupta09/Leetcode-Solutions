b0de468b2885295b01ab38d5e8161ee5008a7654
Ishita Gupta <124298301+ishitagupta09@users.noreply.github.com>
2023-05-30 19:33:43 +0530

    Create Design HashSet.cpp

---
+++ Design HashSet.cpp
@@ -0,0 +1,20 @@
+class MyHashSet {
+ public:
+  MyHashSet() : set(1000001) {}
+
+  void add(int key) {
+    set[key] = true;
+  }
+
+  void remove(int key) {
+    set[key] = false;
+  }
+
+  /** Returns true if this set contains the specified element */
+  bool contains(int key) {
+    return set[key];
+  }
+
+ private:
+  vector<bool> set;
+};
